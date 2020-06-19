c8d17afbab36279543a5b48b1f09da3cd765ea6e
praneeth <k.praneethkumar311@gmail.com>
2020-06-19 12:00:03 +0530

    fine

--- poll_project/urls.py
+++ poll_project/urls.py
@@ -20,7 +20,7 @@ from poll import views as poll_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', poll_views.home, name='home'),
+    path('home/', poll_views.home, name='home'),
     path('create/', poll_views.create, name='create'),
     path('results/<poll_id>/', poll_views.results, name='results'),
     path('vote/<poll_id>/', poll_views.vote, name='vote'),
